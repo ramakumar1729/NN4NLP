725039d91a068bd1e11588c16878665838c3c63f
ramakumar1729 <ramakumar1729@gmail.com>
2017-10-14 19:27:41 -0400

    F1 computation

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import tqdm
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -19,54 +20,27 @@ def prepare_sequence(seq, to_ix):
     return Variable(tensor)
 
 
-def prepare_vocab(sequence):
+def prepare_vocab(sequence, w2i = {}):
 
-    w2i = {"_UNK_": 0, "_PAD_": 1}
-    i2w = ["_UNK_", "_PAD_"]
     for w in sequence:
         if w not in w2i:
             w2i[w] = len(w2i)
-            i2w.append(w)
 
-    return w2i, i2w
+    return w2i
 
 
 def train(args):
-    # training_data = [
-    #                (
-    #                  ("Traditional <C> <entity> information retrieval techniques </entity>"
-    #                   "use a"
-    #                   " <entity> histogram </entity> "
-    #                   "of"
-    #                   " <P> <entity> keywords </entity> "
-    #                   "as the"
-    #                   " <entity> document representation </entity> "
-    #                   "but"
-    #                   " <entity> oral communication </entity> "
-    #                   "may offer additional"
-    #                   " <entity> indices </entity> "
-    #                   "such as the time and place of the rejoinder and the attendance.").split(), ["USAGE"]),
-    #                (
-    #                  ("Traditional <P> <entity> information retrieval techniques </entity>"
-    #                   "use a"
-    #                   " <entity> histogram </entity> "
-    #                   "of"
-    #                   " <C> <entity> keywords </entity> "
-    #                   "as the"
-    #                   " <entity> document representation </entity> "
-    #                   "but"
-    #                   " <entity> oral communication </entity> "
-    #                   "may offer additional"
-    #                   " <entity> indices </entity> "
-    #                   "such as the time and place of the rejoinder and the attendance.").split(), ["NONE"]),
-    #                ]
 
     # import ipdb;ipdb.set_trace()
     trainX, trainY = load_data(os.path.join(args.data_dir, "train.txt"))
     devX, devY = load_data(os.path.join(args.data_dir, "dev.txt"))
 
-    word2idx, idx2word = prepare_vocab([w for i, j in trainX+devX for w in i+j])
-    tag2idx, idx2tag = prepare_vocab([l for l in trainY+devY])
+    w2i = {"_UNK_": 0, "_PAD_": 1}
+    word2idx = prepare_vocab([w for i, j in trainX+devX for w in i+j], w2i = w2i)
+
+    t2i = {"NONE": 0}
+    tag2idx = prepare_vocab([l for l in trainY+devY], w2i = t2i)
+    num_classes = len(tag2idx)
 
     n_batches = len(trainX) // args.batch_size
     dev_n_batches = len(devX) // args.batch_size
@@ -111,6 +85,11 @@ def train(args):
         acc = correct / len(trainX)
 
         # Dev
+        # Compute F-1.
+        TP = [0 for _ in range(num_classes)]
+        FP = [0 for _ in range(num_classes)]
+        FN = [0 for _ in range(num_classes)]
+
         model.eval()
         correct = 0
         eval_loss = 0
@@ -121,8 +100,27 @@ def train(args):
             predictions = model(ps, p_lens, cs, c_lens)
             loss = loss_function(predictions, ys)
             eval_loss += loss.data[0]
-            correct += torch.sum(torch.eq(predictions.data.max(dim=1)[1],
-                                          ys.data))
+            preds = predictions.data.max(dim=1)[1]
+            correct += torch.sum(torch.eq(preds, ys.data))
+
+            for pred, y in zip(preds, ys.data):
+                if pred == y:
+                    TP[y] += 1
+                else:
+                    FP[pred] += 1
+                    FN[y] += 1
+
+        P = [ float(tp)/(tp+fp) if tp+fp > 0 else 0 for tp,fp in zip(TP, FP)]
+        R = [ float(tp)/(tp+fn) if tp+fn > 0 else 0 for tp,fn in zip(TP, FN)]
+        F1 = [ 2*p*r/(p+r) if p+r > 0 else 0 for p,r in zip(P, R)]
+        macro_F1 = np.mean(F1[1:])
+        print("F1  :",F1)
+        print("TP  :",TP)
+        print("FP  :",FP)
+        print("FN  :", FN)
+        print("P  :", P)
+        print("R  :", R)
+        print("Macro-Average F1: {}".format(macro_F1))
         eval_acc = correct / len(devX)
         tqdm.tqdm.write(("Epoch {:2d}\ttr_loss/acc: ({:.3f} | {:.2f})"
                          "\tdv_loss/acc: ({:.3f} | {:.2f})").format(
